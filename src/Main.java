c3b7f56c3cb5eaae985c816e15365e7ece7aba4e
NetoIQA75 <all-slavin@yandex.ru>
2023-11-09 15:20:31 +0300

    ModifiedBonusMiles

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+    }
+}
